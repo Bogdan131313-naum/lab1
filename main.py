52f9587e467c2105b8dbd80b189b54b830f2b999
Bogdan131313-naum <bogdannaumluk0@gmail.com>
2025-09-17 14:01:13 +0300

    add lab1 code

---
+++ main.py
@@ -0,0 +1,20 @@
+name = "Bogdan"
+age = 16 
+km = 22.2
+favourite_movies = ["Inception", "Interstellar", "Avatar"]
+daily_tasks = []
+student = True
+has_driver_license = False
+important_years = (2015, 2010, 2020)
+visited_countries = {"Ukraine", "Poland", "Germany"}
+contacts = {"mom": "099-123-52-34", "friend": "066-555-65-56"}
+print(name, type(name))
+print(age, type(age))
+print(km, type(km))
+print(favourite_movies, type(favourite_movies))
+print(daily_tasks, type(daily_tasks))
+print(student, type(student))
+print(has_driver_license, type(has_driver_license))
+print(important_years, type(important_years))
+print(visited_countries, type(visited_countries))
+print(contacts, type(contacts))
